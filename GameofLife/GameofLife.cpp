c48f510964dac49414ba449b0277a4d8ad616ccb
YoungKnight337 <warfire9145@gmail.com>
2025-06-25 20:47:56 -0400

    Updating main class

--- GameofLife/GameofLife.cpp
+++ GameofLife/GameofLife.cpp
@@ -5,6 +5,16 @@
 
 int main()
 {
-    
+    constexpr int SCREEN_WIDTH = 800;
+    constexpr int SCREEN_HEIGHT = 700;
+    constexpr int FRAME_RATE = 60;
+
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Space Shooter");
+    SetTargetFPS(FRAME_RATE);
+
+    Grid Board;
+
+    Board.Iniitialize();
+
 }
 
