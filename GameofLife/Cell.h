37cebb4217af4454c5cfa03f8bcfdb6f05b51723
YoungKnight337 <warfire9145@gmail.com>
2025-06-25 20:18:04 -0400

    Added some functions

--- GameofLife/Cell.h
+++ GameofLife/Cell.h
@@ -1,5 +1,17 @@
 #pragma once
+#include "raylib.h"
+
 class Cell
 {
+private:
+Vector2 position;
+	bool alive;
+	Color color;
+public:
+	Cell();
+	~Cell();
+	void Initialize();
+	void Update();
+	void Draw();
 };
 
