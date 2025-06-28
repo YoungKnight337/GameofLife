3e898d89639f2c8c2b48bec7fb45870da82cba5d
YoungKnight337 <warfire9145@gmail.com>
2025-06-28 16:45:05 -0400

    Added comment rules

--- GameofLife/Cell.cpp
+++ GameofLife/Cell.cpp
@@ -23,4 +23,16 @@ void Cell::Draw()
 void Cell::Update()
 {
 
+	//1. A live cell with fewer than two live neighbors dies. 
+	// if(active && neighbors < 2)
+	//	active = false;
+	//2. A live cell with more than three live neighbors also dies.
+	// if(active && neighbors > 3)
+	//	active = false;
+	//3. A live cell with exactly two or three live neighbors lives.
+	// if(active && (neighbors == 2 || neighbors == 3))
+	//	active = true;
+	//4. A dead cell with exactly three live neighbors becomes alive.
+	// if(!active && neighbors == 3)
+	//	acitve = true;
 }
