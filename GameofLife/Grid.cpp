e34f7c62445ad8466a4b2a8b086930c4e4806423
YoungKnight337 <warfire9145@gmail.com>
2025-07-03 21:55:54 -0400

    Small Change

--- GameofLife/Grid.cpp
+++ GameofLife/Grid.cpp
@@ -25,7 +25,13 @@ void Grid::Iniitialize()
 
 void Grid::Draw()
 {
-	
+	for (int i = 0; i < row; i++)
+	{
+		for (int j = 0; j < column; j++)
+		{
+			Cell cell(false);
+		}
+	}
 }
 
 void Grid::Update()
