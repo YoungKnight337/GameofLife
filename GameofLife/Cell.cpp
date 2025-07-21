f162f1ac011829da1ed0812231009411cfc3d1ce
YoungKnight337 <warfire9145@gmail.com>
2025-07-21 16:11:27 -0400

    simple change

--- GameofLife/Cell.cpp
+++ GameofLife/Cell.cpp
@@ -52,6 +52,7 @@ void Cell::Rules()
 
 void Cell::GetNeighbors()
 {
+	return; // Function not implemented 
 	//Return a list of neighboring blocks.
 	//Neighbors can be horizontally, vertically, or diagonally adjacent so a block in the center of the board would have 8 neighbors,  while one in a corner would only have 3.
 	//There are many ways of approaching this problem, so take a few minutes to devise a good algorithm, and then try to implement it.
