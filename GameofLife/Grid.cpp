978c7610c3ac2f7541a2071010bbd8acb9479943
YoungKnight337 <warfire9145@gmail.com>
2025-07-23 15:02:07 -0400

    Small Change

--- GameofLife/Grid.cpp
+++ GameofLife/Grid.cpp
@@ -17,7 +17,8 @@ void Grid::Iniitialize(int a, int b)
 	//We wish to make a Block to each square on the board, and store that Block in a dictionary(self.block list).
 	//Remember from Exercise 4.3 that Blocks can be any color listed in rgb.txt!
 	//Read the comments in the code and think about the best way to do this.
-	//Note that you just want to create the Block; you do not want to set it to live!Also, be sure to delete the raise Exception line.
+	//Note that you just want to create the Block; you do not want to set it to live!
+	//Also, be sure to delete the raise Exception line.
 
 	Seed();
 	for (int i = 0; i < a; i++)
