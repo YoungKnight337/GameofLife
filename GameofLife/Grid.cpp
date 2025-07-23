e6ec49a034308debabb52d73a04c8ac4a49fbe28
YoungKnight337 <warfire9145@gmail.com>
2025-07-23 14:53:25 -0400

    Small Change

--- GameofLife/Grid.cpp
+++ GameofLife/Grid.cpp
@@ -45,11 +45,10 @@ void Grid::Seed()
 	//Plant Active Cells within the Grid
 
 
-	//hich takes in a Python list of block coordinate tuples 
-	//something like[(x1, y1), (x2, y2)] and sets the Blocks in those positions to be live.If you get
-	//stuck, review the implementation of random seed to see how we can get the blocks to be visible on
-	//the board; however, you should not use the same for loop that random seed uses hint : your loop
-	//should involve the parameter block coords..
+	//Which takes in a Python list of block coordinate tuples 
+	//something like[(x1, y1), (x2, y2)] and sets the Blocks in those positions to be live.
+	//If you get stuck, review the implementation of random seed to see how we can get the blocks to be visible on the board.
+	//However, you should not use the same for loop that random seed uses hint : your loop should involve the parameter block coords..
 }
 
 void Grid::Update(int a, int b)
