cbb4a4fc3861dbff086dedc3d39a2611aaa8a631
YoungKnight337 <warfire9145@gmail.com>
2025-07-30 15:26:34 -0400

    Removed comments in Rules()

--- GameofLife/Cell.cpp
+++ GameofLife/Cell.cpp
@@ -73,20 +73,15 @@ void Cell::Activate()
 }
 void Cell::Rules()
 {
-	//if active
 	if (active)
 	{
-		//if neighbors are less than two or more than three
 		if (neighbors < 2 || neighbors > 3)
 			active = false;
-		// if neighbors are exactly two or three
 		if (neighbors == 2 || neighbors == 3)
 			active = true;
 	}
-	//if (!active)
 	else
 	{
-		//A dead cell with exactly three live neighbors becomes alive.
 		if (neighbors == 3)
 			active = true;
 	}
