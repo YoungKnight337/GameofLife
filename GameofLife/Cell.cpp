b31acc5945bdfdf324b430f84f41ccf26237b3b8
YoungKnight337 <warfire9145@gmail.com>
2025-08-02 20:54:08 -0400

    Small CHange

--- GameofLife/Cell.cpp
+++ GameofLife/Cell.cpp
@@ -22,7 +22,6 @@ void Cell::Draw()
 void Cell::Update()
 {
 	GetNeighbors();
-	CheckNeighbors();
 	Rules();
 }
 
@@ -95,7 +94,8 @@ void Cell::GetNeighbors()
 
 	//Neighbors can be horizontally, vertically, or diagonally adjacent so a block in the center of the board would have 8 neighbors,  while one in a corner would only have 3.
 	//There are many ways of approaching this problem, so take a few minutes to devise a good algorithm, and then try to implement it.
+	CheckNeighbors();
 }
 
-//
+
 
