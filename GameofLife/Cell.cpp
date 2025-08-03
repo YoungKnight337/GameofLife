bc3a2da60d1f20056ae88d6d392e9534643529a2
YoungKnight337 <warfire9145@gmail.com>
2025-08-02 20:45:29 -0400

    Changed format

--- GameofLife/Cell.cpp
+++ GameofLife/Cell.cpp
@@ -13,45 +13,6 @@ Cell::~Cell()
 
 }
 
-int Cell::CheckNeighbors()
-{
-
-	//Conway's Game of Life uses the 8-cell Moore neighborhood, which includes the diagonal neighbors. 
-	/*
-		Input: A square tessellation, T, containing a connected component P of black cells.
-		Output: A sequence B (b1, b2, ..., bk) of boundary pixels i.e. the contour.
-		Define M(a) to be the Moore neighborhood of pixel a.
-		Let p denote the current boundary pixel.
-		Let c denote the current pixel under consideration i.e. c is in M(p).
-		Let b denote the backtrack of c (i.e. neighbor pixel of p that was previously tested)
- 
-		Begin
-			Set B to be empty.
-			From bottom to top and left to right scan the cells of T until a black pixel, s, of P is found.
-			Insert s in B.
-			Set the current boundary point p to s i.e. p=s
-			Let b = the pixel from which s was entered during the image scan.
-			Set c to be the next clockwise pixel (from b) in M(p).
-			While c not equal to s do
-			{
-				if (c is black)
-				{
-					insert c in B
-					Let b = p
-					Let p = c
-					(backtrack: move the current pixel c to the pixel from which p was entered)
-					Let c = next clockwise pixel (from b) in M(p).
-				}
-				else
-				{
-					(advance the current pixel c to the next clockwise pixel in M(p) and update backtrack)
-					Let b = c
-					Let c = next clockwise pixel (from b) in M(p).
-					end If
-				}
-			}
-	*/
-}
 void Cell::Draw()
 {
 	if(active)
@@ -83,6 +44,46 @@ void Cell::Rules()
 		if (neighbors == 3)
 			active = true;
 	}
+}
+
+int Cell::CheckNeighbors()
+{
+
+	//Conway's Game of Life uses the 8-cell Moore neighborhood, which includes the diagonal neighbors. 
+	/*
+		Input: A square tessellation, T, containing a connected component P of black cells.
+		Output: A sequence B (b1, b2, ..., bk) of boundary pixels i.e. the contour.
+		Define M(a) to be the Moore neighborhood of pixel a.
+		Let p denote the current boundary pixel.
+		Let c denote the current pixel under consideration i.e. c is in M(p).
+		Let b denote the backtrack of c (i.e. neighbor pixel of p that was previously tested)
+
+		Begin
+			Set B to be empty.
+			From bottom to top and left to right scan the cells of T until a black pixel, s, of P is found.
+			Insert s in B.
+			Set the current boundary point p to s i.e. p=s
+			Let b = the pixel from which s was entered during the image scan.
+			Set c to be the next clockwise pixel (from b) in M(p).
+			While c not equal to s do
+			{
+				if (c is black)
+				{
+					insert c in B
+					Let b = p
+					Let p = c
+					(backtrack: move the current pixel c to the pixel from which p was entered)
+					Let c = next clockwise pixel (from b) in M(p).
+				}
+				else
+				{
+					(advance the current pixel c to the next clockwise pixel in M(p) and update backtrack)
+					Let b = c
+					Let c = next clockwise pixel (from b) in M(p).
+					end If
+				}
+			}
+	*/
 }
 
 void Cell::GetNeighbors()
