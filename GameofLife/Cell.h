570c9f1ed478f0f976dba2e29d370fee6351fa11
YoungKnight337 <warfire9145@gmail.com>
2025-08-12 11:20:04 -0400

    Small Change

--- GameofLife/Cell.h
+++ GameofLife/Cell.h
@@ -15,13 +15,13 @@ private:
 public:
 	Cell();
 	~Cell();
-	int CheckNeighbors();
 	void Update();
 	void Draw();
 	void Rules();
 	void GetNeighbors();
 	
 	Vector2 GetPosition() { return position; }
+	int CheckNeighbors();
 	bool GetState(bool active) { return active; }
 	bool SetState(bool active) { this->active = active; }
 };
