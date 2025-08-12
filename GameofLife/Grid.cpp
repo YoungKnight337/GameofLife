44c5cc41fa47ed7bfa21c996eff0c91f1e386a45
YoungKnight337 <warfire9145@gmail.com>
2025-08-12 16:24:21 -0400

    Small Change

--- GameofLife/Grid.cpp
+++ GameofLife/Grid.cpp
@@ -42,9 +42,10 @@ void Grid::Draw(int a, int b)
 
 void Grid::Seed()
 {
+	int neighbors;
 	//Plant Active Cells within the Grid
 	cell.GetPosition() = {i , j};
-	if(cell.GetNeighbors(3) && cell.GetState(true))
+	if(cell.GetNeighbors(neighbors)==3 && cell.GetState(true))
 		cell.SetState(true);
 	//Which takes in a list of block coordinate tuples something like[(x1, y1), (x2, y2)] and sets the Blocks in those positions to be live.
 	//If you get stuck, review the implementation of random seed to see how we can get the blocks to be visible on the board.
