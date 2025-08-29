a815336d097d23bf70fc3e8fd7bc082f5c21dfd2
YoungKnight337 <warfire9145@gmail.com>
2025-08-28 21:56:12 -0400

    Small Change

--- GameofLife/Grid.cpp
+++ GameofLife/Grid.cpp
@@ -16,7 +16,6 @@ void Grid::Iniitialize(int a, int b)
 	//In the init method of the Board class, you need to finish laying out the board. Currently, only the board is drawn.
 	//We wish to make a Block to each square on the board, and store that Block in a dictionary(self.block list).
 	//Note that you just want to create the Block; DO NOT SET TO LIVE
-	//Also, be sure to delete the raise Exception line.
 
 	for (int i = 0; i < a; i++)
 	{
@@ -45,8 +44,14 @@ void Grid::Seed()
 	float j = cell.GetPosition().y;
 	//Plant Active Cells within the Grid
 	{ i, j; }
-	if(cell.GetNeighbors(neighbors) == 3 && cell.GetState(true))
+	if (cell.GetNeighbors(neighbors) == 3 && cell.GetState(true))
 		cell.SetState(true);
+	if (cell.GetNeighbors(neighbors == 2) && cell.GetState(true))
+		cell.SetState(true);
+	if(cell.GetNeighbors(neighbors < 2) && cell.GetState(true))
+		cell.SetState(false);
+	if (cell.GetNeighbors(neighbors > 3) && cell.GetState(true))
+		cell.SetState(false);
 	//Which takes in a list of block coordinate tuples something like[(x1, y1), (x2, y2)] and sets the Blocks in those positions to be live.
 	//If you get stuck, review the implementation of random seed to see how we can get the blocks to be visible on the board.
 	//However, you should not use the same for loop that random seed uses hint : your loop should involve the parameter block coords..
