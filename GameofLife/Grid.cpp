cca94bd03f7a7ac5fb8ed5276ac783864ed81ba7
YoungKnight337 <warfire9145@gmail.com>
2025-09-02 14:48:44 -0400

    Small Changes

--- GameofLife/Grid.cpp
+++ GameofLife/Grid.cpp
@@ -62,9 +62,9 @@ void Grid::Seed()
 	//Which takes in a list of block coordinate tuples something like[(x1, y1), (x2, y2)] and sets the Blocks in those positions to be live.
 	//If you get stuck, review the implementation of random seed to see how we can get the blocks to be visible on the board.
 	//However, you should not use the same for loop that random seed uses hint : your loop should involve the parameter block coords..
-	for(int i = 0; i < 0 ; i++)
+	for(int i = 0; i < a ; i++)
 	{
-		for (int j = 0; j < 0; j++)
+		for (int j = 0; j < b; j++)
 		{
 			cell.SetState(true); //cell[i][j].SetState(true);
 		}
