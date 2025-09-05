f810d9b95e878021f2e529ef8f6b96980bd5a18e
YoungKnight337 <warfire9145@gmail.com>
2025-09-05 19:37:35 -0400

    Clean Up Code

--- GameofLife/GameofLife.cpp
+++ GameofLife/GameofLife.cpp
@@ -8,9 +8,6 @@
 
 int main()
 {
-    //constexpr int SCREEN_WIDTH = 800;
-    //constexpr int SCREEN_HEIGHT = 450;
-    //constexpr int FRAME_RATE = 60;
 
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Space Shooter");
     SetTargetFPS(FRAME_RATE);
