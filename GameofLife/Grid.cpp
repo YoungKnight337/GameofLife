1b6c1ed50888455c784e60d491709cc487d90831
YoungKnight337 <warfire9145@gmail.com>
2025-09-05 19:37:15 -0400

    Small Change

--- GameofLife/Grid.cpp
+++ GameofLife/Grid.cpp
@@ -27,9 +27,9 @@ void Grid::Iniitialize()
 
 void Grid::Draw()
 {
-	for (int i = 0; i < a; i++)
+	for (size_t i = 0; i < N; i++)
 	{
-		for (int j = 0; j < b ; j++)
+		for (size_t j = 0; j < N ; j++)
 		{
 			cell.Draw(); //cell[a][b].Draw();
 		}
