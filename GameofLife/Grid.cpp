adca11a3f3288c1bf4de1ea14e0ff4de6084ec0d
YoungKnight337 <warfire9145@gmail.com>
2025-09-06 19:47:46 -0400

    Small Changes

--- GameofLife/Grid.cpp
+++ GameofLife/Grid.cpp
@@ -59,16 +59,15 @@ void Grid::Seed()
 void Grid::CheckNeighbors()
 {
 	//Check each cell that is alive alongside it neighbors
-	//If cell is active while having 2 and 3 cells it stays active
-	//	cell.GetNeighbors(neighbors);[i][j]
-	// if(cell.GetNeighbors(neighbors) == 3 && cell.GetState(true))
-	// 		cell.SetState(true); //cell[a][b].SetState(true);
-	// else
-	//		cell.SetState(false); //cell[a][b].SetState(false);
-	// if(cell.GetNeighbors(neighbors) == 2 && cell.GetState(true))
-	// 		cell.SetState(true); //cell[a][b].SetState(true);
-	// else
-	//		cell.SetState(false); //cell[a][b].SetState(false);
+	if(cell.GetNeighbors(neighbors) == 3 && cell.GetState(true))
+		cell.SetState(true); //cell[a][b].SetState(true);
+	else
+		cell.SetState(false); //cell[a][b].SetState(false);
+	
+	if(cell.GetNeighbors(neighbors) == 2 && cell.GetState(true))
+		cell.SetState(true); //cell[a][b].SetState(true);
+	else
+		cell.SetState(false); //cell[a][b].SetState(false);
 }
 
 void Grid::Update()
