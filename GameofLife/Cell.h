1be8af74acb31ef591fdd7f5e2e7c02141bebc6c
YoungKnight337 <warfire9145@gmail.com>
2025-09-09 18:16:47 -0400

    Small Change

--- GameofLife/Cell.h
+++ GameofLife/Cell.h
@@ -17,10 +17,10 @@ public:
 	Cell();
 	~Cell();
 	void Draw();
-	int GetNeighbors(int neighbors);
 	
 	Vector2 GetPosition() { return position; }
-	////int CheckNeighbors();
+
+	int GetNeighbors(int neighbors);
 	bool GetState(bool active) { return active; }
 	bool SetState(bool active) { this->active = active; }
 };
