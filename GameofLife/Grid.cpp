1cf18a62278630cc0f62e8d936f2fbe310fd6f76
YoungKnight337 <warfire9145@gmail.com>
2025-09-09 21:10:30 -0400

    Note Changes

--- GameofLife/Grid.cpp
+++ GameofLife/Grid.cpp
@@ -11,10 +11,14 @@ Grid::~Grid()
 
 void Grid::Iniitialize()
 {
-	//In the init method of the Board class, you need to finish laying out the board. Currently, only the board is drawn.
-	//We wish to make a Block to each square on the board, and store that Block in a dictionary(self.block list).
-	//Note that you just want to create the Block; DO NOT SET TO LIVE
-	// Current Solution: Simple set Cell to False once instantiated.
+	//In the init method of the Board class, you need to finish laying out the board. 
+	//Currently, only the board is drawn.
+	
+	//We wish to make a Block to each square on the board: Board is composed of cells that are either active or dead.
+	
+	//Store that Block in a dictionary(self.block list): Store Cell in Array?
+	
+	//Note that you just want to create the Block; DO NOT SET TO LIVE: Simply set Cell to False once instantiated.
 
 	for (size_t i = 0; i < N; i++)
 	{
@@ -45,8 +49,11 @@ void Grid::Seed()
 	//Plant Active Cells within the Grid
 	//{ i, j; }
 	//Which takes in a list of block coordinate tuples something like[(x1, y1), (x2, y2)] and sets the Blocks in those positions to be live.
+	
 	//If you get stuck, review the implementation of random seed to see how we can get the blocks to be visible on the board.
+	
 	//However, you should not use the same for loop that random seed uses hint : your loop should involve the parameter block coords..
+
 	for(size_t i = 0; i < N ; i++)
 	{
 		for (size_t j = 0; j < N; j++)
@@ -81,6 +88,7 @@ void Grid::Update()
 	{
 		for (size_t j = 0; j < N; j++)
 		{
+			Seed();
 			CheckNeighbors();
 		}
 	}
