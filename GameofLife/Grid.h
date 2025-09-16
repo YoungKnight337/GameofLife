98f3d7956bbd2f1d837a3e333145606c0e786d14
YoungKnight337 <warfire9145@gmail.com>
2025-09-15 21:52:16 -0400

    Feedback from Blackhalo117

--- GameofLife/Grid.h
+++ GameofLife/Grid.h
@@ -26,3 +26,19 @@ public:
 //[Blackhalo117]
 //Yeah, the Grid class would, in my mind, be a class that stores it's data into an array, 
 //It also offers you the methods for stepping through the cells and creating a new array every cycle through
+
+
+/*
+	Oh so like, a typical array would be something like this:
+	var fuu = []
+	
+	To make it multi-dimensional, you might try something like this:
+	var fuu = []
+	for i in range(0, 10):
+      temp = []
+      fuu.append(temp)
+
+	Now you can access "cells" inside of this by typing:
+	fuu[x][y] = new cell()
+	So x is your width and y is your height, like pixels :3
+*/
